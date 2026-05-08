9e106ad93e1da91717865f455b96d78cf2e01782
Vanqui <92575737+Vanqui123z@users.noreply.github.com>
2026-05-08 11:44:27 +0700

    Update chatbot.js

--- server/src/utils/chatbot.js
+++ server/src/utils/chatbot.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-
+console.log(process.env.GEMINI_MODEL);
+console.log(process.env.GEMINI_API_KEY);
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);

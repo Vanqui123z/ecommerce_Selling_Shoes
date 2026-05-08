cc96afef6b18bd5756864ead5bac0799acd08646
Vanqui <92575737+Vanqui123z@users.noreply.github.com>
2026-05-08 09:56:46 +0700

    Use environment variables for API key and model
    Refactor chatbot.js to use environment variables for API key and model.

--- server/src/utils/chatbot.js
+++ server/src/utils/chatbot.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const { GoogleGenerativeAI } = require('@google/generative-ai');
-const genAI = new GoogleGenerativeAI('AIzaSyDAypfQFznnEcIyun8npzFBwGSiJtE1vBk');
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const model = genAI.getGenerativeModel({
+    model: process.env.GEMINI_MODEL
+});
 
 const modelProduct = require('../models/ModelProducts');
 
